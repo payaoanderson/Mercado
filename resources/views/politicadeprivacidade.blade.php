2a16423544d3c9c677121c3eaadc39b21eff0835
Anderson <andersonpayao018@gmail.com>
2024-06-29 18:44:10 -0300

    links

---
+++ resources/views/politicadeprivacidade.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,600;1,100&family=Poppins:wght@500&family=Vina+Sans&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+
+    <title>Mercado Digital</title>
+</head>
+
+<body>
+
+
+
+
+    
+    
+<div class="cabeça">
+    <header id="cabecalho">
+        <nav class="container">
+            <div class="bloco-texto">
+                <ul>
+                    <li>
+                       <img src="img/logo.jpg" width="100">
+
+                    </li>
+                </ul>
+
+                <div class="bloco-texto2">
+                    <ul>
+                        <li>
+                            <a href="index.html">Mercado Digital</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="bloco-pesquisa">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="pesquisa">
+                    <form action="mais-pesquisado.html" onsubmit="return enviarFormulario()" id="meuFormulario">
+                        <input type="text" id="meuCampo" placeholder="pesquisa" onkeydown="handleKeyDown(event)" />
+                        <input type="submit" value="seach" id="botaoEnviar" />
+                      </form>
+                   
+                   
+                </div>
+
+            </div>
+
+            <div class="bloco-carrinho">
+                <ul>
+                    <li>
+                        <a href="#"><i id="fa-solid fa-cart-shopping"></i></a>
+                    </li>
+                </ul>
+
+
+            </div>
+
+        </nav>
+
+    </header>
+</div>
+
+    <main class="container">
+       
+
+        <h1 class="texto">Política de Privacidade do Mercado Digital</h1>
+
+        <p>
+            Informações de Identificação Pessoal: Isso pode incluir seu nome, endereço, endereço de e-mail, número de telefone e outras informações semelhantes que você fornece voluntariamente ao criar uma conta ou preencher formulários em nosso site.
+            Informações de Pagamento: Se você realizar transações através do nosso site, podemos coletar informações de pagamento, como detalhes do cartão de crédito ou outras formas de pagamento.
+            Informações de Navegação: Podemos coletar informações sobre o seu uso do nosso site, incluindo o tipo de navegador que você usa, o tempo gasto em nossas páginas e outras informações de navegação.
+            Uso das Informações
+            
+            Utilizamos as informações coletadas para os seguintes propósitos:
+            
+            Personalização da Experiência do Usuário: Podemos utilizar suas informações para personalizar sua experiência em nosso site e apresentar conteúdo e ofertas específicas para você.
+            Processamento de Transações: Utilizamos suas informações de pagamento para processar transações que você realiza através do nosso site.
+            Comunicação: Podemos utilizar suas informações de contato para enviar-lhe comunicações sobre o nosso site, incluindo atualizações, novidades e ofertas especiais. Você pode optar por não receber essas comunicações a qualquer momento.
+            Proteção das Informações
+            
+            Implementamos medidas de segurança para proteger suas informações contra acesso não autorizado ou divulgação. No entanto, é importante lembrar que nenhum método de transmissão pela internet ou armazenamento eletrônico é 100% seguro e, portanto, não podemos garantir a segurança absoluta de suas informações.
+            
+            Compartilhamento de Informações
+            
+            Não compartilhamos suas informações pessoais com terceiros, exceto conforme necessário para processar transações ou fornecer serviços que você solicitou.
+            
+            Alterações na Política de Privacidade
+            
+            Podemos atualizar esta Política de Privacidade periodicamente. Recomendamos que você revise esta página regularmente para se manter informado sobre as nossas práticas de privacidade.
+            
+            Ao utilizar o Mercado Digital, você concorda com os termos desta Política de Privacidade. Se você não concordar com esta Política de Privacidade, por favor, não utilize nosso site.
+            
+            Se você tiver dúvidas sobre esta Política de Privacidade, entre em contato conosco através das informações fornecidas em nosso site.
+            
+            Data de Vigência: [Inserir data de vigência da política]
+            
+            Obrigado por escolher o Mercado Digital. Estamos comprometidos em proteger sua privacidade e proporcionar uma experiência segura e confiável.
+        </p>
+
+    </main>
+
+
+    <footer id="rodape">
+        <P class="texto-rodape">&copy; <span id="ano"></span> TODOS OS DIREITOS RESEVADOS AO MERCADO DIGITAL</p>
+            <h6 class="nome">EMAIL: digitalmercado@mercadodigitalstore.com</h6>
+
+
+     <script src="./js/script.js"></script>
+
+</body>
+
+</html>
+
